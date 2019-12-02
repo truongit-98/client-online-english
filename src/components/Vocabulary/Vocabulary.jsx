2754bf7e1719d5f5d8c8998accd5adc9612d9023
nguyentrung031 <trung9977.mta@gmail.com>
2019-12-02 11:44:07 +0700

    fix indent

--- src/components/Vocabulary/Vocabulary.jsx
+++ src/components/Vocabulary/Vocabulary.jsx
@@ -5,39 +5,39 @@ import './Vocabulary.css';
 import ItemsTopic from './ItemsTopic';
 
 class Vocabulary extends Component {
-	render() {
-		const {topicItem} = this.props.vocabulary;
-		return (
-			<div className="vocabulary-wrap">
-				<div className="vocabulary-item-title">
-					<div className="container">
-						<h2 className="Title-text">Học từ vựng tiếng Anh theo chủ đề - Online Engline</h2>
-						<p className="Title-item-text">Gồm 100+ khóa học từ vựng tiếng Anh theo chủ đề, phù hợp cho mọi đối tượng</p>
-						<div className="list-vocabulary">
-							<Link to={`/ListVocabulary/`}>Danh sách tất cả từ vựng<i className="fas fa-angle-down ml-2"></i></Link>
-						</div>
-					</div>
-				</div>
-				<div className="container body">
-					<div id="title-1">
-						<p>Danh sách chủ đề</p>
-					</div>
-					<div className="row">
-						{
-							topicItem.map((value, key) => 
-							<ItemsTopic key={key} {...value}></ItemsTopic>
-							)
-						}
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const {topicItem} = this.props.vocabulary;
+    return (
+      <div className="vocabulary-wrap">
+        <div className="vocabulary-item-title">
+          <div className="container">
+            <h2 className="Title-text">Học từ vựng tiếng Anh theo chủ đề - Online Engline</h2>
+            <p className="Title-item-text">Gồm 100+ khóa học từ vựng tiếng Anh theo chủ đề, phù hợp cho mọi đối tượng</p>
+            <div className="list-vocabulary">
+              <Link to={`/ListVocabulary/`}>Danh sách tất cả từ vựng<i className="fas fa-angle-down ml-2"></i></Link>
+            </div>
+          </div>
+        </div>
+        <div className="container body">
+          <div id="title-1">
+            <p>Danh sách chủ đề</p>
+          </div>
+          <div className="row">
+            {
+              topicItem.map((value, key) => 
+              <ItemsTopic key={key} {...value}></ItemsTopic>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state, ownProps) => {
-	return {
-		vocabulary: state.vocabulary
-	}
+  return {
+    vocabulary: state.vocabulary
+  }
 }
 
 export default connect(mapStateToProps)(Vocabulary);
--- src/components/Vocabulary/VocabularySearch.jsx
+++ src/components/Vocabulary/VocabularySearch.jsx
@@ -6,61 +6,61 @@ class VocabularySearch extends Component {
     var snd = new Audio(this.props.location.state.pronounce);
     snd.play();
   }
-	render() {
-		const {item} = this.props.location.state;
-		return (
-			<div className="vocabulary-wrap">
-				<div className="vocabulary-item-title">
-					<div className="container">
-						<h2 className="Title-text">Kết quả tìm kiếm - Online Engline</h2>
-						<p className="Title-item-text">khóa học từ vựng tiếng Anh theo chủ đề, phù hợp cho mọi đối tượng</p>
-					</div>
-				</div>
-				<div className="container body">
-					<div className="row">
-					{
-						item.map((value, key)  => (
-							<div className="col-md-3 search" key={key} data-toggle="modal" data-target={"#exampleModalCenter" + value.vocaID}>
-								<div className="card">
-									<div className="card-body">
-										<div className="card-icon">
-											<i className="fas fa-book"/>
-										</div>
-										<h3 className="card-Title">{value.word}</h3>
-										<p className="card-Content">Hệ thống học từ vựng tiếng Anh</p>
-										<hr className="hr-card"/>
-										<div className="card-link">Xem chi tiết</div>
-									</div>
-								</div>
-							</div>
-							)
-						)
-					}
-					</div>
-				</div>
-				{/* Modal */}
-				{
-					item.map((value, key) => (
-						<div className="modal fade" id={"exampleModalCenter" + value.vocaID} tabIndex={-1} role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" key={key}>
-							<div className="modal-dialog modal-dialog-centered" role="document">
-								<div className="modal-content" >
-									<div className="modal-header">
-										<h5 className="modal-title" id="exampleModalCenterTitle">Chi tiết từ vựng:</h5>
-									</div>
-									<div className="modal-body">
-										<h6><span>Từ: </span>{value.word}</h6>
-										<h6><span>Phiên âm: </span>{value.transcribe}</h6>
-										<h6><span>Nghĩa: </span>{value.meanWord}</h6>
-										<h6 onClick={this.onClickAudio}><span>pronounce: </span><i className="fas fa-volume-up ml-3"></i></h6>
-									</div>
-								</div>
-							</div>
-						</div>
-					))
-				}
-			</div>
-		);
-	}
+  render() {
+    const {item} = this.props.location.state;
+    return (
+      <div className="vocabulary-wrap">
+        <div className="vocabulary-item-title">
+          <div className="container">
+            <h2 className="Title-text">Kết quả tìm kiếm - Online Engline</h2>
+            <p className="Title-item-text">khóa học từ vựng tiếng Anh theo chủ đề, phù hợp cho mọi đối tượng</p>
+          </div>
+        </div>
+        <div className="container body">
+          <div className="row">
+          {
+            item.map((value, key)  => (
+              <div className="col-md-3 search" key={key} data-toggle="modal" data-target={"#exampleModalCenter" + value.vocaID}>
+                <div className="card">
+                  <div className="card-body">
+                    <div className="card-icon">
+                      <i className="fas fa-book"/>
+                    </div>
+                    <h3 className="card-Title">{value.word}</h3>
+                    <p className="card-Content">Hệ thống học từ vựng tiếng Anh</p>
+                    <hr className="hr-card"/>
+                    <div className="card-link">Xem chi tiết</div>
+                  </div>
+                </div>
+              </div>
+              )
+            )
+          }
+          </div>
+        </div>
+        {/* Modal */}
+        {
+          item.map((value, key) => (
+            <div className="modal fade" id={"exampleModalCenter" + value.vocaID} tabIndex={-1} role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true" key={key}>
+              <div className="modal-dialog modal-dialog-centered" role="document">
+                <div className="modal-content" >
+                  <div className="modal-header">
+                    <h5 className="modal-title" id="exampleModalCenterTitle">Chi tiết từ vựng:</h5>
+                  </div>
+                  <div className="modal-body">
+                    <h6><span>Từ: </span>{value.word}</h6>
+                    <h6><span>Phiên âm: </span>{value.transcribe}</h6>
+                    <h6><span>Nghĩa: </span>{value.meanWord}</h6>
+                    <h6 onClick={this.onClickAudio}><span>pronounce: </span><i className="fas fa-volume-up ml-3"></i></h6>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))
+        }
+      </div>
+    );
+  }
 }
 
 export default VocabularySearch;
