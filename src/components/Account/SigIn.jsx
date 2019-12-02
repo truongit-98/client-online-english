4aedbda7feb41ff7aee446d8bcda7f15f4988f14
nguyentrung031 <trung9977.mta@gmail.com>
2019-12-02 17:16:07 +0700

    fin indent

--- src/components/Account/SigIn.jsx
+++ src/components/Account/SigIn.jsx
@@ -4,15 +4,15 @@ import './Account.css';
 // import { login } from '../../store/Action/authAction';
 
 class SigIn extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			email: '',
-			password: '',
-			msg: null
-		}
-	}
-	onChange = e => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      msg: null
+    }
+  }
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
   onSubmit = e => {
@@ -23,43 +23,43 @@ class SigIn extends Component {
     //   password
     // };
   };
-	render() {
-		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-						<div className="card card-signin my-5">
-							<div className="card-body b1">
-								<h5 className="card-title text-center">Sign In</h5>
-								<form className="form-signin" onSubmit={this.onSubmit}>
-									<div className="form-label-group">
-										<input type="email" name="email" id="inputEmail" className="form-control" placeholder="Email address" onChange={this.onChange} required autofocus />
-										<label htmlFor="inputEmail">Email address</label>
-									</div>
-									<div className="form-label-group">
-										<input type="password" name="password" id="inputPassword" className="form-control" placeholder="Password" onChange={this.onChange} required />
-										<label htmlFor="inputPassword">Password</label>
-									</div>
-									<button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
-									<hr className="my-4" />
-									<button className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i className="fab fa-google mr-2" /> Sign in with Google</button>
-									<button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i className="fab fa-facebook-f mr-2" /> Sign in with Facebook</button>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card card-signin my-5">
+              <div className="card-body b1">
+                <h5 className="card-title text-center">Sign In</h5>
+                <form className="form-signin" onSubmit={this.onSubmit}>
+                  <div className="form-label-group">
+                    <input type="email" name="email" id="inputEmail" className="form-control" placeholder="Email address" onChange={this.onChange} required autofocus />
+                    <label htmlFor="inputEmail">Email address</label>
+                  </div>
+                  <div className="form-label-group">
+                    <input type="password" name="password" id="inputPassword" className="form-control" placeholder="Password" onChange={this.onChange} required />
+                    <label htmlFor="inputPassword">Password</label>
+                  </div>
+                  <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign in</button>
+                  <hr className="my-4" />
+                  <button className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i className="fab fa-google mr-2" /> Sign in with Google</button>
+                  <button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i className="fab fa-facebook-f mr-2" /> Sign in with Facebook</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state, ownProps) => {
-	return {
-		error: state.error
-	}
+  return {
+    error: state.error
+  }
 }
 const mapDispatchToProps = {
-	
+  
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(SigIn);
--- src/components/Account/SingUp.jsx
+++ src/components/Account/SingUp.jsx
@@ -5,16 +5,16 @@ import './Account.css';
 // import { login } from '../../store/Action/authAction';
 
 class SigUp extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			name: '',
-			email: '',
-			password: '',
-			msg: null
-		}
-	}
-	onChange = e => {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+      msg: null
+    }
+  }
+  onChange = e => {
     this.setState({ [e.target.name]: e.target.value });
   };
   onSubmit = e => {
@@ -29,44 +29,44 @@ class SigUp extends Component {
     // // Attempt to register
     // this.props.register(newUser);
   };
-	render() {
-		return (
-			<div className="container">
-				<div className="row">
-					<div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-						<div className="card card-signin my-5">
-							<div className="card-body b1">
-								<h5 className="card-title text-center">Sign up</h5>
-								<form className="form-signin" onSubmit={this.onSubmit}>
-									<div className="form-label-group">
-										<input type="text" name="name" id="name" className="form-control" placeholder="Tên tài khoản" onChange={this.onChange} required autofocus />
-										<label htmlFor="name">Tên tài khoản</label>
-									</div>
-									<div className="form-label-group">
-										<input type="email" name="email" id="inputEmail" className="form-control" placeholder="Email address" onChange={this.onChange} required autofocus />
-										<label htmlFor="inputEmail">Email address</label>
-									</div>
-									<div className="form-label-group">
-										<input type="password" name="password" id="inputPassword" className="form-control" placeholder="Password" onChange={this.onChange} required />
-										<label htmlFor="inputPassword">Password</label>
-									</div>
-									<button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign up</button>
-									<hr className="my-4" />
-									<button className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i className="fab fa-google mr-2" /> Sign in with Google</button>
-									<button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i className="fab fa-facebook-f mr-2" /> Sign in with Facebook</button>
-								</form>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+            <div className="card card-signin my-5">
+              <div className="card-body b1">
+                <h5 className="card-title text-center">Sign up</h5>
+                <form className="form-signin" onSubmit={this.onSubmit}>
+                  <div className="form-label-group">
+                    <input type="text" name="name" id="name" className="form-control" placeholder="Tên tài khoản" onChange={this.onChange} required autofocus />
+                    <label htmlFor="name">Tên tài khoản</label>
+                  </div>
+                  <div className="form-label-group">
+                    <input type="email" name="email" id="inputEmail" className="form-control" placeholder="Email address" onChange={this.onChange} required autofocus />
+                    <label htmlFor="inputEmail">Email address</label>
+                  </div>
+                  <div className="form-label-group">
+                    <input type="password" name="password" id="inputPassword" className="form-control" placeholder="Password" onChange={this.onChange} required />
+                    <label htmlFor="inputPassword">Password</label>
+                  </div>
+                  <button className="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Sign up</button>
+                  <hr className="my-4" />
+                  <button className="btn btn-lg btn-google btn-block text-uppercase" type="submit"><i className="fab fa-google mr-2" /> Sign in with Google</button>
+                  <button className="btn btn-lg btn-facebook btn-block text-uppercase" type="submit"><i className="fab fa-facebook-f mr-2" /> Sign in with Facebook</button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 const mapStateToProps = (state, ownProps) => {
-	return {
-		error: state.error
-	}
+  return {
+    error: state.error
+  }
 }
 const mapDispatchToProps = {
 
